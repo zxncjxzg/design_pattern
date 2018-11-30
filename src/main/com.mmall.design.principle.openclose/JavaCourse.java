7a3f0ebc765868ae6afdf8c5c848b83bb0c2d60a
1179074459 <1179074459@qq.com>
2018-11-30 12:04:30 +0800

    开闭原则代码实践

---
+++ src/main/com.mmall.design.principle.openclose/JavaCourse.java
@@ -0,0 +1,33 @@
+/**
+ * Created by 11790 on 2018/11/28.
+ */
+
+/**
+ * Java课程类
+ */
+public class JavaCourse implements ICourse {
+    private Integer id;
+    private String name;
+    private Double price;
+
+    public JavaCourse(Integer id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public Integer getId() {
+        return this.id;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public Double getPrice() {
+        return this.price;
+    }
+}
