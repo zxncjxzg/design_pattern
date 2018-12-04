3e9e4eefa585d291a916743d5749617eaab674f0
1179074459 <1179074459@qq.com>
2018-12-04 13:10:27 +0800

    重新开始编码

--- src/main/com.mmall.design.principle.openclose/Test.java
+++ src/main/com.mmall.design.principle.openclose/Test.java
@@ -2,9 +2,4 @@
  * Created by 11790 on 2018/11/28.
  */
 public class Test {
-    public static void main(String[] args) {
-        ICourse javaCourse=new JavaDiscountCourse(96,"java从零到企业级电商开发",348d);
-
-        System.out.println("课程ID:"+javaCourse.getId()+",课程名称："+javaCourse.getName()+"，课程价格："+javaCourse.getPrice()+"元");
-    }
 }
